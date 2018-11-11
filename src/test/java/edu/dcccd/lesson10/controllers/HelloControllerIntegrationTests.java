decb48dc754bea1e49f304dd1b9ba934e02c5c3b
e2646718 <e2646718@student.dcccd.edu>
2018-11-11 17:49:21 -0500

    Initial commit.

--- src/test/java/edu/dcccd/lesson10/controllers/HelloControllerIntegrationTests.java
+++ src/test/java/edu/dcccd/lesson10/controllers/HelloControllerIntegrationTests.java
@@ -10,9 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloController.class)
--- src/test/java/edu/dcccd/lesson10/controllers/HelloControllerUnitTests.java
+++ src/test/java/edu/dcccd/lesson10/controllers/HelloControllerUnitTests.java
@@ -1,6 +1,9 @@
 package edu.dcccd.lesson10.controllers;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.ui.Model;
 import org.springframework.validation.support.BindingAwareModelMap;
 
